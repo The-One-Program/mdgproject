141ba0d9216d74628c90823def94d871afd6ff4e
The-One-Program <kshitiz4kaushik@gmail.com>
2022-03-20 20:40:51 +0530

    Mdg project

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _state = false;
+  // int x = 0;
 
   @override
   void initState() {
     super.initState();
     ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
-      _state = !_state;
-      print("Hello");
+      setState(() {
+        _state = !_state;
+        // print("Hello");
+      });
     });
   }
 
@@ -36,15 +39,63 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
-        Container(
-          color: _state ? Colors.black : Colors.red,
-          height: 400,
-          width: 500,
+        Expanded(
+          child: Container(
+            color: _state ? Colors.cyan[300] : Colors.red[400],
+          ),
         ),
         Container(
-          color: _state ? Colors.red : Colors.black,
-          height: 400,
-          width: 500,
+          color: Colors.white,
+          height: 2,
+        ),
+        Container(
+          color: Colors.black, //Not needed
+          height: 100,
+          child: Row(children: [
+            Expanded(
+              child: Container(
+                color: _state ? Colors.amber : Colors.black,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: _state ? Colors.black : Colors.amber,
+                height: 100,
+                child: Icon(
+                  Icons.accessibility,
+                  color: _state ? Colors.pink : Colors.deepPurple[600],
+                ),
+              ),
+            ),
+            Container(
+              color: _state ? Colors.white : Colors.pink,
+              width: 2,
+            ),
+            Expanded(
+              child: Container(
+                color: _state ? Colors.black : Colors.amber,
+                height: 100,
+                child: Icon(
+                  Icons.accessibility,
+                  color: _state ? Colors.deepPurple[600] : Colors.pink,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: _state ? Colors.amber : Colors.black,
+              ),
+            )
+          ]),
+        ),
+        Container(
+          color: Colors.white,
+          height: 2,
+        ),
+        Expanded(
+          child: Container(
+            color: _state ? Colors.red[400] : Colors.cyan[300],
+          ),
         ),
       ]),
     );
